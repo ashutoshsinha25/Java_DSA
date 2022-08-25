6df7758441bb89250b3056ad3286de059ed4157e
ashutosh <ashutoshsinha519@gmail.com>
2022-08-25 17:13:34 +0530

    update

--- 8.Linked List/linkedList.java
+++ 8.Linked List/linkedList.java
@@ -1,45 +1,49 @@
-// import java.util.*;
-// import java.io.*;
+public class linkedList{
+    // public static class Person{
+    //     String name;
+    //     int age;
+    // }
 
 
-// public class linkedList {
-//     public static class Node{
-//         int data;
-//         Node next;
+    public static class Node{
+        int data;
+        Node next;
 
-//     }
+    }
 
-//     public static class LinkedList{
-//         Node head;
-//         Node tail;
-//         int size;
-        
-//     }
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
 
-//     public static void testList(LinkedList list){
-//         for(Node temp = list.head ; temp !=null ; temp = temp.next){
-//             System.out.println(temp.data);
-//         }
-//         System.out.println(list.size);
+        void addLast(int data){
+            Node n = new Node();
+            n.data = data;
 
-//         if(list.size > 0){
-//             System.out.println(list.tail.data);
-//         }
-//     }
+            if(size == 0){
+                head = tail = n;
+            }else{
+                tail.next = n;
+                tail = n;
+            }
+            size++;
+        } 
+    }
+    public static void main(String[] args) {
+        // Person p1 = new Person();
+        // System.out.println(p1.name + " " + p1.age);
+        // p1.name = "Ashutosh";
+        // p1.age = 20;
+        // System.out.print(p1.name + " " + p1.age);
 
-//     public static void main(String[] args){
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//         LinkedList list = new LinkedList();
+        LinkedList ll = new LinkedList();
+        System.out.println(ll.head + " " +  ll.tail + " " + ll.size);
+        ll.addLast(10);
+        ll.addLast(20);
+        ll.addLast(30);
+        System.out.println(ll.head.data + " " + ll.tail.data + " " + ll.size);
 
-//         String str = br.readLine();
-//         while(str.equals("quit") == false){
-//             if(str.startsWith("addLast")){
-//                 int val = Integer.parseInt(str.split(" ")[1]);
-//                 list.addLast(val);
-//             } 
-//             str = br.readLine();
-//         }
 
-//         testList(list);
-//     }
-// }
+
+    }
+}
