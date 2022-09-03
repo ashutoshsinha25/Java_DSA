33f2ac39ada63ac09c997f579a75ba7cdd8b7e55
ashutosh <ashutoshsinha519@gmail.com>
2022-09-04 03:16:51 +0530

    first

---
+++ Level2/3.Tree/preorderTraversal.java
@@ -0,0 +1,58 @@
+//// 144. Binary Tree Preorder Traversal
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }lef
+ * }
+*/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        }
+    }
+public class preorderTraversal {
+    public ArrayList<Integer> preorder(TreeNode root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        while(root != null){
+            if(root.left == null){ // root can be visited only once
+                ans.add(root.val);
+                root = root.right;
+            }else{ // root can be visited twice 
+                TreeNode  rootp1 = root.left;
+                while(rootp1.right != null && rootp1.right != root){
+                    rootp1 = rootp1.right;
+                }
+                if(rootp1.right == null){ // first visit
+                    ans.add(root.val);
+                    rootp1.right = root; // connect
+                    root = root.left;
+                }else if(rootp1.right == root){ // second visit
+                    
+                    rootp1.right = null; // disconnect
+                    root = root.right;
+                    
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
