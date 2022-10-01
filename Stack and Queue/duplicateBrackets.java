c8bcc256814a50e00a822e49a1c9b6e66e3f6477
ashutosh <ashutoshsinha519@gmail.com>
2022-10-01 20:59:05 +0530

    first

---
+++ Stack and Queue/duplicateBrackets.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main{
+   public static void duplicateBrackets(String args[]){
+      Scanner scn = new Scanner(System.in);
+      String exp = scn.nextLine();
+      System.out.println(checkDuplicate(exp));
+   }
+
+   public static boolean checkDuplicate(String exp){
+      Stack<Character> st = new Stack<>();
+
+      for(int i=0 ; i<exp.length() ; i++){
+         char ch = exp.charAt(i);
+         if(ch == ' ') continue;
+         else if(ch == ')'){
+               if(st.peek() == '('){
+                  return true;
+               }else{
+                  while(st.peek() != '('){
+                     st.pop(); // remove middle contents
+                  }
+                  st.pop(); // opening bracket
+               }
+         }else{
+            st.push(ch);
+         }
+      }
+
+      return false;
+   }
+}
