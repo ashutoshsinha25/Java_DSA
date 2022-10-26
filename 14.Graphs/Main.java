425e0e046383798581ae8400c499999999be5ae3
ashutosh <ashutoshsinha519@gmail.com>
2022-10-26 11:13:28 +0530

    update

--- 14.Graphs/Main.java
+++ 14.Graphs/Main.java
@@ -11,7 +11,13 @@ public class Main {
     }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int nvtces = scn.nextInt(); // first thig passed
+        // list(scn);
+        matrix(scn);
+        
+    }
+
+    public static void list(Scanner scn){
+        int nvtces = scn.nextInt(); // first thing passed
         ArrayList<Edge>[] graph = new ArrayList[nvtces];
         // above array would have default value of null in all blocks
         // initialize with empty ararylist
@@ -32,6 +38,25 @@ public class Main {
                 System.out.println(edge.src + "-->" + edge.nbr);
             }
         }
+    }
 
+    public static void matrix(Scanner scn){
+        int nvtces = scn.nextInt();
+        int nedges = scn.nextInt();
+        int[][] adjmatrix = new int[nvtces][nvtces];
+        for(int i = 1 ; i <= nedges ; i++){
+            int v1 = scn.nextInt() , v2 = scn.nextInt();
+            adjmatrix[v1][v2] = 1;
+            adjmatrix[v2][v1] = 1;
+        }
+
+        scn.close();
+        
+        for(int i = 0 ; i < nvtces ; i++){
+            for(int j = 0 ; j < nvtces ; j++){
+                System.out.print(adjmatrix[i][j] +" ");
+            }
+            System.out.println();
+        }
     }
 }
