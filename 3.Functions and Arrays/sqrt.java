a5619188df79e8aee05c0f65d07256129a8750ac
ashutosh <ashutoshsinha519@gmail.com>
2023-07-17 02:21:25 +0530

    first commit

---
+++ 3.Functions and Arrays/sqrt.java
@@ -0,0 +1,16 @@
+// import java.util.*;
+// public class sqrt {
+//     public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         int n = scn.nextInt();
+//         scn.close();
+//         System.out.print("Square root of " + n + " is :" + root(n));
+//     }
+//     private static double root(int x){
+//         if(x<0) return x;
+
+
+
+
+//     }
+// }
